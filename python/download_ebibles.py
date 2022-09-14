c9c6b2328711db8245a1ccfec346219fca2a835b
David <davidcbaines@gmail.com>
2022-09-14 10:01:02 +0100

    Combine downloaded and unzipping in download_ebibles.py

--- python/download_ebibles.py
+++ python/download_ebibles.py
@@ -68,7 +68,40 @@ def download_zip_file(url, headers, save_as):
 
         return save_as
     return None
+    
+def get_folder(file):
+    # Get the path of the folder to which the zip file should be extracted."
+    return unzipped_folder / file.name[0: (len(file.name) - len(file_suffix))]
 
+
+def get_tree_size(path):
+    """Return total size of files in given path and subdirs."""
+    total = 0
+    for entry in os.scandir(path):
+        if entry.is_dir(follow_symlinks=False):
+            total += get_tree_size(entry.path)
+        else:
+            total += entry.stat(follow_symlinks=False).st_size
+    return total
+    
+    
+def unzip_ebibles(source_folder, file_suffix, dest_folder):
+    pattern = "*" + file_suffix
+    zip_files = [zip_file for zip_file in source_folder.glob(pattern)]
+    
+    # Strip off the pattern so that the subfolder name is the project ID.
+    extract_folders = [ (zip_file, get_folder(zip_file)) for zip_file in zip_files ]
+    extracts = [ (zip_file, folder) for zip_file, folder in extract_folders if zip_file.stat().st_size >= get_tree_size(folder) ]
+
+    print(
+        f"Found {len(zip_files)} files in {source_folder} matching pattern: {pattern}"
+    )
+    print(f"Found {len(extracts)} that are smaller than the zip file.")
+    
+    for zip_file, extract in extracts:
+        extract.mkdir(parents=True, exist_ok=True)
+        print(f"Extracting to: {extract}")
+        shutil.unpack_archive(zip_file, extract)
 
 def main():
     # Set logging level
@@ -166,6 +199,7 @@ def main():
             else:
                 print(f"Could not download {url}\n")
 
+    unzip_ebibles(zips_folder, file_suffix, unzipped_folder)
 
 
 if __name__ == "__main__":
